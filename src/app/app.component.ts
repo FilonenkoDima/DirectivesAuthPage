9bc9c4bc2deac27f6ee6b470061b762d8ee3f730
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2024-11-25 14:25:26 +0200

    added auth component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { LearningResourcesComponent } from './learning-resources/learning-resources.component';
+import { AuthComponent } from './auth/auth.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LearningResourcesComponent],
+  imports: [LearningResourcesComponent, AuthComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
