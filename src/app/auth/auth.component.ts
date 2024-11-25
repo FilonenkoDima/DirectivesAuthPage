648f81d7d5b0f7052248a8c0c5dc0cafc6263e29
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2024-11-25 14:37:46 +0200

    add auth logic

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './auth.component.html',
-  styleUrl: './auth.component.css'
+  styleUrl: './auth.component.css',
 })
 export class AuthComponent {
+  email = signal('');
+  password = signal('');
+  private authService = inject(AuthService);
 
+  onSubmit() {
+    this.authService.autheticate(this.email(), this.password());
+  }
 }
