1782565c6a1c7a2d6db9192536586f8676d9c89f
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2024-11-25 15:15:22 +0200

    added custom component safeLink

--- src/app/learning-resources/learning-resources.component.ts
+++ src/app/learning-resources/learning-resources.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { SafeLinkDirective } from '../safe-link.directive';
 
 @Component({
   selector: 'app-learning-resources',
   standalone: true,
-  imports: [],
+  imports: [SafeLinkDirective],
   templateUrl: './learning-resources.component.html',
-  styleUrl: './learning-resources.component.css'
+  styleUrl: './learning-resources.component.css',
 })
-export class LearningResourcesComponent {
-
-}
+export class LearningResourcesComponent {}
