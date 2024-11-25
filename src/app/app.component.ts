7c16b0c88617a109b6c2b3c32777930eb46c9354
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2024-11-25 15:00:56 +0200

    structural directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { LearningResourcesComponent } from './learning-resources/learning-resources.component';
 import { AuthComponent } from './auth/auth.component';
+import { AuthService } from './auth/auth.service';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [LearningResourcesComponent, AuthComponent],
+  imports: [LearningResourcesComponent, AuthComponent, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'AuthPage';
+
+  private authService = inject(AuthService);
+
+  isAdmin = computed(() => this.authService.activePermission() === 'admin');
 }
